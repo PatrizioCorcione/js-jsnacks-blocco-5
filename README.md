e6cc004e83187feccb09ff21ef3446ce88216a05
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-07 13:57:55 +0100

    [end] prima e seconda mileston

--- README.md
+++ README.md
@@ -4,12 +4,15 @@ Beast of five
 ## Sviluppo
 - Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
 Calcola quanto pesano tutte le zucchine.
+
 - Crea 10 oggetti che rappresentano una zucchina.
 Dividi in due array separati le zucchine che misurano meno o più di 15cm.
 Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
 - Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 -  Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro
 es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
 - Scrivi una funzione che accetti tre argomenti:
 un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
-La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”.
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+///////////////////milestone 1
+
 const zucchine = [
   {
     varietà: "zucchino nero di Milano",
@@ -32,7 +34,7 @@ const zucchine = [
   {
     varietà: "zucchino calabrese",
     peso: 30,
-    lunghezza: 0.5
+    lunghezza: 18
   },
   {
     varietà: "zucchino sardo",
@@ -42,11 +44,43 @@ const zucchine = [
   {
     varietà: "zucchino ligure",
     peso: 10,
-    lunghezza: 0.1
+    lunghezza: 30
   },
   {
     varietà: "zucchino veneto",
     peso: 5,
-    lunghezza: 0.05
+    lunghezza: 17
   }
 ]
+
+pesoTot = 0;
+
+zucchine.forEach((zucchina) => {
+  let {peso} = zucchina;
+  pesoTot = pesoTot + peso;
+  
+});
+console.log(pesoTot);
+
+//////////////////////milestone 2
+let zucchineLunghe = [];
+let zucchineCorte = [];
+pesoTotCorte = 0;
+pesoTotLunghe = 0;
+
+zucchine.forEach(element => {
+  if (element.lunghezza > 15) {
+    zucchineLunghe.push(element);
+    pesoTotLunghe = pesoTotLunghe + element.peso;
+  } else {
+    zucchineCorte.push(element);
+    pesoTotCorte = pesoTotCorte + element.peso;
+  }
+});
+
+console.log(zucchineCorte);
+console.log(zucchineLunghe);
+console.log(pesoTotCorte);
+console.log(pesoTotLunghe);
+
+
