1e96e5e0c4094deba55858c491e5d4879b4cb667
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-07 13:32:16 +0100

    [README ZUCCHINO]

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const zucchine = [
+  {
+    varietà: "zucchino nero di Milano",
+    peso: 500,
+    lunghezza: 20
+  },
+  {
+    varietà: "zucchino romanesco", 
+    peso: 400,
+    lunghezza: 15
+  },
+  {
+    varietà: "zucchino ortolano di Faenza",
+    peso: 300,
+    lunghezza: 10
+  },
+  {
+    varietà: "zucchino siciliano",
+    peso: 200,
+    lunghezza: 5
+  },
+  {
+    varietà: "zucchino toscano",
+    peso: 100,
+    lunghezza: 2
+  },
+  {
+    varietà: "zucchino pugliese",
+    peso: 50,
+    lunghezza: 1
+  },
+  {
+    varietà: "zucchino calabrese",
+    peso: 30,
+    lunghezza: 0.5
+  },
+  {
+    varietà: "zucchino sardo",
+    peso: 20,
+    lunghezza: 0.2
+  },
+  {
+    varietà: "zucchino ligure",
+    peso: 10,
+    lunghezza: 0.1
+  },
+  {
+    varietà: "zucchino veneto",
+    peso: 5,
+    lunghezza: 0.05
+  }
+]
